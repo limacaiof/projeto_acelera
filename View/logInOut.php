b9496f72a7461be43519f0e69eb6bbe367071fcb
RUAN RITA <ruan.rita@fatec.sp.gov.br>
2020-11-10 16:56:29 -0300

    Login  e Cadastro Pronto

---
+++ View/logInOut.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../src/css/logInOut.css">
+    <link rel="stylesheet" href="../src/css/fonts.css">
+    <title>logInOut</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="modalLogin">
+            <h1>Logar</h1>
+            <hr>
+            <form action="Controller/" method="POST">
+                <input class="camp" type="text" name="email" id="emailId" placeholder="email">
+                <input class="camp" type="password" name="senha" id="senhaId" placeholder="senha">
+                <div class="form-check ckbox">
+                    <input type="checkbox" class="box" id="boxId">
+                    <p class="boxtxt">Manter-me Logado</p>
+                </div>
+                <button type="submit" class="btn btn-enviar">Entrar</button>
+                <div class="footer-modal">
+                    <h2 class="semconta">Ainda não tem conta?</h2>
+                    <button class="btn-cad" type="button" onclick="mudarModal()">Cadastra-se</button>
+                </div>
+            </form>
+        </div>
+        <div class="modalCadastrar">
+            <div class="row">
+                <div class="banner col-sm-5">
+                    <h1>Seja Bem Vindo ! ! !</h1>
+                    <div class="logo">
+                        <img src="logotipo" alt="">
+                    </div>
+                </div>
+                <div class="account col-sm-7">
+                    <h1>Criar Conta</h1>
+                    <hr>
+                    <input class="camp" type="text" name="nome" id="nomeId" placeholder="nome">
+                    <input class="camp" type="text" name="email" id="emailId" placeholder="email">
+                    <input class="camp" type="password" name="senha" id="senhaId" placeholder="senha">
+                    <button type="submit" class="btn btn-enviar">Entrar</button>
+                    <div class="footer-modal">
+                        <h2 class="semconta">Ainda não tem conta?</h2>
+                        <button class="btn-cad" type="button" onclick="mudarModal()">Cadastra-se</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
