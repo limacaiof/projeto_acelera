05f26bdf624db948d9cf8d41a630f33168a9822f
caioDeodato <caio.deodato@yahoo.com>
2020-11-27 22:36:26 -0300

    adicionando 2 models

---
+++ Model/Evento.php
@@ -0,0 +1,16 @@
+<?php 
+
+    class Evento {
+
+        public $id_evento;
+        public $nome_evento;
+        public $data_evento;
+        public $data_cadastro;
+        public $valor_evento;
+        public $descricao;
+        public $email_usuario;
+
+        
+    }
+
+?>
---
+++ Model/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Usuario {
+
+        public $nome;
+        public $email;
+        public $senha;
+        public $valor_inicial;
+        
+    }
+
+?>
