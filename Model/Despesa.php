14a17562974f8e9de8d5307e58ac49561e9e6e62
caioDeodato <caio.deodato@yahoo.com>
2020-12-01 21:27:06 -0300

    adicionando model de despesa

---
+++ Model/Despesa.php
@@ -0,0 +1,14 @@
+<?php
+
+class Despesa {
+    
+    public $id_despesa;
+    public $nome_despesa;
+    public $descricao_despesa;
+    public $data_cadastro;
+    public $data_limite;
+    public $forma_pagamento;
+    public $valor_despesa;
+    public $situacao;
+    public $email_usuario;
+}
