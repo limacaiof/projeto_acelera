4d84ffae512f34ee0429dc6437bc3e278c939782
Ruan-InRita <ruan.f.r.gt@gmail.com>
2020-12-02 22:37:53 -0300

    Editando  Planejar Eventos

--- View/planejarEventos.php
+++ View/planejarEventos.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Palnejar Eventos</title>
+    <title>Planejar Eventos</title>
     <!-- COMPONENTE MENU  KKK  -->
     <?php
         require './componentes/bootstrap4-5-2.php';
@@ -38,18 +38,49 @@
                 <a class="nav-link font2 active" href="#">Planeja Eventos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link font2" href="#">Gastos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link font2" href="#">Investimentos</a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link font2" href="#">Limites</a>
             </li>
         </ul>
     </div>
     <section class="container">
-
+        <div class="titlePageContent">
+            <H1>Meus Eventos</H1>
+        </div>
+        <div class="tableGraphicContent">
+            <div class="tableContent">
+            <table class="table tabela font2">
+                <thead>
+                    <tr>
+                        <th>Nº</th>
+                        <th>Despesas</th>
+                        <th>Valor</th>
+                        <th>Data Vencimento</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Default</td>
+                        <td>Defaultson</td>
+                        <td>def@somemail.com</td>
+                    </tr>
+                    <tr class="">
+                        <td>2</td>
+                        <td>Primary</td>
+                        <td>Joe</td>
+                        <td>joe@example.com</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            <div class="graphicContent">
+                <h1>Custos Mensais</h1>
+                <div id="grafico">
+                    <canvas id="myChart" max-width="100%" max-height="100%"></canvas>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- Footer -->
     <footer class="footer-my">
@@ -59,6 +90,6 @@
     </footer>
 
 </body>
-<script type="text/javascript" src="../src/js/notication.js"></script>
-<script type="text/javascript" src="../src/js/depesas.js"></script>
+    <script type="text/javascript" src="../src/js/notication.js"></script>
+    <script type="text/javascript" src="../src/js/depesas.js"></script>
 </html>
