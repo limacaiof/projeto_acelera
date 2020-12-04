161d0f152eca8a655447ce151402ffaf583250d8
caioDeodato <caio.deodato@yahoo.com>
2020-12-03 23:08:41 -0300

    adicionando funcionalidade cadastro

--- Controller/DespesaPageController.php
+++ Controller/DespesaPageController.php
@@ -1,10 +1,17 @@
 <?php
 
     session_start();
+    date_default_timezone_set('America/Sao_Paulo');
     include('../Controller/DespesaController.php');
     $controller = new DespesaController();
 
-    $acao = $_GET['acao'];
+    $acao = '';
+
+    if(isset($_GET['acao'])) {
+        $acao = $_GET['acao']; 
+    } elseif(isset($_POST['acao'])) {
+        $acao = $_POST['acao'];
+    }
 
     if($acao == 'apagar') {
         $id = $_GET['id'];
@@ -19,4 +26,27 @@
             header('Location: http://localhost/projeto_acelera/View/despesas.php');
             exit();
         }
-    } 
+
+    } elseif($acao == 'cadastrar') {
+        $despesa = new Despesa();
+        $despesa->nome_despesa = $_POST['nome'];
+        $despesa->valor_despesa = $_POST['valor'];
+        $despesa->data_limite = $_POST['data'];
+        $despesa->data_cadastro = date("d/m/Y");
+        $despesa->situacao = $_POST['situacao'];
+        $despesa->forma_pagamento = $_POST['fpagamento'];
+        $despesa->email_usuario = $_POST['email'];
+
+        $resultado = $controller->salvarDespesa($despesa);
+
+        if($resultado) {
+            $_SESSION['msg'] = "Despesa cadastrada com sucesso!";
+            header('Location: http://localhost/projeto_acelera/View/despesas.php');
+            exit();
+
+        } else {
+            $_SESSION['msg-erro'] = "Ocorreu um erro ao realizar operação, tente novamente!";
+            header('Location: http://localhost/projeto_acelera/View/despesas.php');
+            exit();
+        }
+    }
